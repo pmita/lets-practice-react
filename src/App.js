da7c51e1dbe2bdee4e1ddb78c0e915d16ba5b933
panos.mita2@outlook.com <pmita@dxc.com>
2021-05-04 13:18:20 +0100

    sass and packages set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//Let's import our css
+import './styles/app.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React configured</h1>
     </div>
   );
 }
